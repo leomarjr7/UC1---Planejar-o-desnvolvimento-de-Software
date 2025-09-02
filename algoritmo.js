1039637cf1445a2ad1d59ae5be1b73f071875ae4
leomarjr7 <leomarjunior.1204@gmail.com>
2025-09-01 21:54:24 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+// Fazendo café
+primeiroPasso = "1. Coloque a cereal na tijela \n"
+segundoPasso = "2. Pegue o leite \n"
+terceiroPasso = "3. Despeja-o na tijela \n"
+quartoPasso = "4. Pegue uma colher \n"
+quintoPasso = "5. Se divirta! \n"
+
+// utilizando o console.log para exibir as informações
+console.log(primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso)
---
+++ bolo.js
@@ -0,0 +1,13 @@
+//Receita bolo 
+primeiroPasso = "1. Prepare a massa do bolo e asse ela à 180° graus \n"
+segundoPasso = "2. Corte a massa em 3 camadas \n"
+terceiroPasso = "3. Higienize a mesa e passe manteiga nela \n"
+quartoPasso = "4. Para o recheio misture morango com nata \n"
+quintoPasso = "5. Bote a primeira camada de massa e coloque em cima da mesa \n"
+sextoPasso = "6. Irrigue a massa com leite \n "
+setimoPasso = "7. Passe o recheio de morango com nata na primeira camada \n"
+oitavoPasso = "8. Repita esse processo três vezes \n"
+nonoPasso = "9. Espalhe chantilly em cima do bolo e deixe uniforme (de preferencia de forma bonita) \n"
+decimoPasso = "10. Jogue morangos de forma decorativa em cima do bolo \n"
+decimopreimeiroPasso = "11. Sirva sua fatia!"
+console.log (primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso,sextoPasso,setimoPasso,oitavoPasso,nonoPasso,decimoPasso,decimoPasso)
